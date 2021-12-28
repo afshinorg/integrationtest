dc42bf4e0d8464c61af2b4997691b3d9577778f7
Afshin Ghazi <onlinestudio22@gmail.com>
2021-12-28 18:31:08 +0200

    Basket api + repository layer + redis add

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Repositories;
+
+public interface IBasketRepository
+{
+    Task<ShoppingCart> GetBasket(string userName);
+    // No create since we are using Redis -> use key and whole object to create -> Update whole basket
+    Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+    Task DeleteBasket(string userName);
+}
+
