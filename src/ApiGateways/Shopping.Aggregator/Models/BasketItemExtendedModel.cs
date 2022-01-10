37b46fff774825fc8466c029f29a0a8cc3493ef8
Afshin Ghazi <onlinestudio22@gmail.com>
2022-01-10 22:13:29 +0200

    Aggregator Models

--- src/ApiGateways/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
@@ -2,13 +2,14 @@
 {
     public class BasketItemExtendedModel
     {
+        // From ShoppingCartItem
         public int Quantity { get; set; }
         public string Color { get; set; }
         public decimal Price { get; set; }
         public string ProductId { get; set; }
         public string ProductName { get; set; }
 
-        //Product Related Additional Fields
+        //Product Related Additional Fields from CatalogModel
         public string Category { get; set; }
         public string Summary { get; set; }
         public string Description { get; set; }
--- src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
+++ src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
@@ -1,14 +1,13 @@
-﻿namespace Shopping.Aggregator.Models
+﻿namespace Shopping.Aggregator.Models;
+// Similar to Catalog.API -> Entities -> Product
+public class CatalogModel
 {
-    // Similar to Catalog.API -> Entities -> Product
-    public class CatalogModel
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public string Summary { get; set; }
-        public string Description { get; set; }
-        public string ImageFile { get; set; }
-        public decimal Price { get; set; }
-    }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Summary { get; set; }
+    public string Description { get; set; }
+    public string ImageFile { get; set; }
+    public decimal Price { get; set; }
 }
+
